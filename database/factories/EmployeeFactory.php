b504a18580edb0cb6af956edc0206c6d1180272a
Yohannes Mekonnen <johannesm1219@gmail.com>
2022-07-12 22:49:39 +0300

    add employee model with blueprint

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Employee;
+
+class EmployeeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Employee::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'id_number' => $this->faker->word,
+            'birthdate' => $this->faker->dateTime(),
+            'salary' => $this->faker->numberBetween(-10000, 10000),
+        ];
+    }
+}
